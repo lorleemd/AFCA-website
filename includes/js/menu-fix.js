a8e4e0fd46fe499657df13c2e10a935a0a6679ea
bpsweeney <brandon.p.sweeney@gmail.com>
2025-03-31 12:25:04 -0800

    mobile dropdown update.
    updating the mobile dropdown.

--- includes/js/menu-fix.js
+++ includes/js/menu-fix.js
@@ -1,3 +1,24 @@
+window.addEventListener('resize', function () {
+    const screenWidth = window.innerWidth;
+    const menuIcon = document.querySelector('.fusion-icon-bars');
+    const mobileMenu = document.getElementById('mobile-menu-afc');
+    const parentNav = mobileMenu.closest('.fusion-mobile-menu-text-align-left');
+    const submenuButtons = document.querySelectorAll('.fusion-open-submenu');
+
+    if (screenWidth >= 800) {
+        menuIcon.setAttribute('aria-expanded', 'false');
+        mobileMenu.style.display = 'none';
+        parentNav.style.display = 'none';
+        parentNav.setAttribute('aria-expanded', 'false');
+
+        submenuButtons.forEach(button => {
+            button.setAttribute('aria-expanded', 'false');
+            const submenu = button.nextElementSibling;
+            submenu.style.display = 'none';
+        });
+    }
+});
+
 document.addEventListener('DOMContentLoaded', function () {
     const menuIcon = document.querySelector('.fusion-icon-bars');
     const mobileMenu = document.getElementById('mobile-menu-afc');
@@ -7,11 +28,31 @@ document.addEventListener('DOMContentLoaded', function () {
             event.preventDefault(); // Prevent default anchor action
             const isExpanded = menuIcon.getAttribute('aria-expanded') === 'true';
             
+            const parentNav = mobileMenu.closest('.fusion-mobile-menu-text-align-left');
+
             // Toggle the menu
             menuIcon.setAttribute('aria-expanded', !isExpanded);
-            mobileMenu.style.display = isExpanded ? 'none' : 'block';
+            mobileMenu.style.display = isExpanded ? 'none' : 'contents';
+            parentNav.setAttribute('aria-expanded', !isExpanded);
+            parentNav.style.display = isExpanded ? 'none' : 'block';
         });
     } else {
         console.error('Menu icon or mobile menu not found in the DOM.');
     }
+
+    const submenuButtons = document.querySelectorAll('.fusion-open-submenu');
+    
+    submenuButtons.forEach(button => {
+        button.addEventListener('click', function (event) {
+            event.preventDefault(); // Prevent default anchor action
+            const isExpanded = button.getAttribute('aria-expanded') === 'true';
+            
+            // Get the nearest submenu
+            const submenu = button.nextElementSibling;
+            
+            // Toggle the submenu
+            button.setAttribute('aria-expanded',!isExpanded);
+            submenu.style.display = isExpanded ? 'none' : 'block';
+        });
+    });
 });
